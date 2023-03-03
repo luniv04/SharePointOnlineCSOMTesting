3caecdd39338ace56bc73cf95238df8887a78dfc
Christopher Stratmann <clstratmann@gmail.com>
2023-03-03 16:33:48 -0500

    changes.

--- CSOMTesting/Program.cs
+++ CSOMTesting/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.SharePoint.Client;
 using PnP.Framework;
 using System.Net.Http.Headers;
 
-var scopes = new[] { "https://78q4t7.sharepoint.com/.default" };
+var scopes = new[] { "https://microsoft.sharepoint-df.com/.default" };
 var tenantId = "6397730c-2970-4413-b48f-1abf00895d39";
 var clientId = "826f164a-3c6c-4fdc-ba36-ed01442c1008";
 var clientSecret = "";
@@ -17,67 +17,127 @@ var clientApplication = ConfidentialClientApplicationBuilder.Create(clientId)
 var result = await clientApplication.AcquireTokenForClient(scopes).ExecuteAsync();
 var bearerToken = result.AccessToken;
 
-//Example1(siteUrl, clientId, bearerToken);
+Example1(siteUrl, clientId, bearerToken);
 await Example2(siteUrl, bearerToken);
 await Example3(siteUrl, bearerToken);
 await Example4(siteUrl, clientId, clientSecret, bearerToken, tenantId);
 await Example5(siteUrl, bearerToken);
 
-
 static void Example1(string siteUrl, string clientId, string bearerToken)
 {
-	using (var ctx = new AuthenticationManager().GetACSAppOnlyContext(siteUrl, clientId, bearerToken))
+	try
 	{
-		ctx.Load(ctx.Web, web => web.Title);
-		ctx.ExecuteQuery();
-		Console.WriteLine(ctx.Web.Title);
+		using (var ctx = new AuthenticationManager().GetACSAppOnlyContext(siteUrl, clientId, bearerToken))
+		{
+			ctx.Load(ctx.Web, web => web.Title);
+			ctx.ExecuteQuery();
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(ctx.Web.Title);
+			Console.ResetColor();
+		}
+	}
+	catch(Exception ex)
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine($"Example1 Failed: {ex.Message}");
+		Console.ResetColor();
 	}
 }
 
 static async Task Example2(string siteUrl, string bearerToken)
 {
-	using (var client = new HttpClient())
+	try
 	{
-		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
-		client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json;odata=nometadata");
+		using (var client = new HttpClient())
+		{
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+			client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json;odata=nometadata");
 
-		var rawJson = await client.GetStringAsync($"{siteUrl}/_api/web?$select=Title");
-		Console.WriteLine(rawJson);
+			var rawJson = await client.GetStringAsync($"{siteUrl}/_api/web?$select=Title");
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(rawJson);
+			Console.ResetColor();
+		}
+	}
+	catch (Exception ex)
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine($"Example2 Failed: {ex.Message}");
+		Console.ResetColor();
 	}
 }
 
 static async Task Example3(string siteUrl, string bearerToken)
 {
-	using (var ctx = new AuthenticationManager().GetAccessTokenContext(siteUrl, bearerToken))
+	try
 	{
-		var currentWeb = ctx.Web;
-		ctx.Load(currentWeb);
-		await ctx.ExecuteQueryRetryAsync();
-		Console.WriteLine(currentWeb.Title);
+		using (var ctx = new AuthenticationManager().GetAccessTokenContext(siteUrl, bearerToken))
+		{
+			var currentWeb = ctx.Web;
+			ctx.Load(currentWeb);
+			await ctx.ExecuteQueryRetryAsync();
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(currentWeb.Title);
+			Console.ResetColor();
+		}
+	}
+	catch (Exception ex)
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine($"Example3 Failed: {ex.Message}");
+		Console.ResetColor();
 	}
 }
 
 static async Task Example4(string siteUrl, string clientId, string clientSecret, string bearerToken, string tenantId)
 {
-	using (var ctx = new AuthenticationManager(clientId, clientSecret, new UserAssertion(bearerToken), tenantId).GetContext(siteUrl))
+	try
 	{
-		ctx.Load(ctx.Web, web => web.Title);
-		await ctx.ExecuteQueryAsync();
-		Console.WriteLine(ctx.Web.Title);
+		using (var ctx = new AuthenticationManager(clientId, clientSecret, new UserAssertion(bearerToken), tenantId).GetContext(siteUrl))
+		{
+			ctx.Load(ctx.Web, web => web.Title);
+			await ctx.ExecuteQueryAsync();
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(ctx.Web.Title);
+			Console.ResetColor();
+		}
+	}
+	catch (Exception ex)
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine($"Example4 Failed: {ex.Message}");
+		Console.ResetColor();
 	}
 }
 
 async Task Example5(string siteUrl, string bearerToken)
 {
-	var url = $"{siteUrl}/_api/Web/Lists";
-	var result = string.Empty;
-	using (var client = new HttpClient())
+	try
 	{
-		client.DefaultRequestHeaders.Clear();
-		client.DefaultRequestHeaders.Add("Accept", "application/json;odata=nometadata");
-		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
-		var response = await client.GetAsync(url);
-		response.EnsureSuccessStatusCode();
-		result = await response.Content.ReadAsStringAsync();
+		var url = $"{siteUrl}/_api/Web/Lists";
+		var result = string.Empty;
+		using (var client = new HttpClient())
+		{
+			client.DefaultRequestHeaders.Clear();
+			client.DefaultRequestHeaders.Add("Accept", "application/json;odata=nometadata");
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+			var response = await client.GetAsync(url);
+			response.EnsureSuccessStatusCode();
+			result = await response.Content.ReadAsStringAsync();
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(result);
+			Console.ResetColor();
+		}
+	}
+	catch (Exception ex)
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine($"Example5 Failed: {ex.Message}");
+		Console.ResetColor();
 	}
 }
